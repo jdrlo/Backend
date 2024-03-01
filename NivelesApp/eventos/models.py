31fe5a05d7fac083fcd40c6f6af6bc0eac17daaa
jdrlo <ruizjuliand4@gmail.com>
2024-02-29 22:03:24 -0500

    update

--- NivelesApp/eventos/models.py
+++ NivelesApp/eventos/models.py
@@ -7,7 +7,7 @@ from django.utils.translation import gettext as _
 class Eventos(models.Model):
 
     id_Foto = models.AutoField(primary_key=True)
-    foto = models.ImageField(verbose_name=_("Foto"),null=True, blank=True, upload_to='publicidad/%Y%m/')
+    imagen_Evento = models.ImageField(verbose_name=_("Foto"),null=True, blank=True, upload_to='eventos/%Y%m/')
     precio_Palco = models.IntegerField(verbose_name=_("Precio Palco"),null=True,blank=True)
     cantidad_Personas = models.IntegerField(verbose_name=_("Cantidad Personas"),default=0)
     created = models.DateTimeField(auto_now_add=True)
